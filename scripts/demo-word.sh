6bb6fed0d5d5338728ef73a1cc62ca9f1544321d
Dav Yaginuma <dav@akuaku.org>
2013-08-17 11:38:02 -0700

    Updated first demo script

--- scripts/demo-word.sh
+++ scripts/demo-word.sh
@@ -1,7 +1,18 @@
-make
-if [ ! -e text8 ]; then
-  wget http://mattmahoney.net/dc/text8.zip -O text8.gz
-  gzip -d text8.gz -f
+DATA_DIR=../data
+BIN_DIR=../src
+
+pushd ${BIN_DIR} && make; popd
+
+if [ ! -e $DATA_DIR/text8 ]; then
+  wget http://mattmahoney.net/dc/text8.zip -O $DATA_DIR/text8.gz
+  gzip -d $DATA_DIR/text8.gz -f
 fi
-time ./word2vec -train text8 -output vectors.bin -cbow 0 -size 200 -window 5 -negative 0 -hs 1 -sample 1e-3 -threads 12 -binary 1
-./distance vectors.bin
+
+echo -----------------------------------------------------------------------------------------------------
+echo -- Training...
+time $BIN_DIR/word2vec -train $DATA_DIR/text8 -output $DATA_DIR/vectors.bin -cbow 0 -size 200 -window 5 -negative 0 -hs 1 -sample 1e-3 -threads 12 -binary 1
+
+echo -----------------------------------------------------------------------------------------------------
+echo -- Calculating distance...
+
+$BIN_DIR/distance $DATA_DIR/vectors.bin
