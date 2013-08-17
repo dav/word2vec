92c6614addb1da248968b261046f7ac2f6247e7a
Dav Yaginuma <dav@akuaku.org>
2013-08-17 14:00:23 -0700

    Update for demo word accuracy script, however the utility is set faulting atm

--- scripts/demo-word-accuracy.sh
+++ scripts/demo-word-accuracy.sh
@@ -1,8 +1,28 @@
-make
-if [ ! -e text8 ]; then
-  wget http://mattmahoney.net/dc/text8.zip -O text8.gz
-  gzip -d text8.gz -f
+DATA_DIR=../data
+BIN_DIR=../bin
+
+TEXT_DATA=$DATA_DIR/text8
+VECTOR_DATA=$DATA_DIR/text8-vector.bin
+
+pushd ${BIN_DIR} && make; popd
+
+  
+if [ ! -e $VECTOR_DATA ]; then
+  
+  if [ ! -e $TEXT_DATA ]; then
+    wget http://mattmahoney.net/dc/text8.zip -O $DATA_DIR/text8.gz
+    gzip -d $DATA_DIR/text8.gz -f
+  fi
+  echo -----------------------------------------------------------------------------------------------------
+  echo -- Training vectors...
+  time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow 0 -size 200 -window 5 -negative 0 -hs 1 -sample 1e-3 -threads 12 -binary 1
+  
 fi
-time ./word2vec -train text8 -output vectors.bin -cbow 0 -size 200 -window 5 -negative 0 -hs 1 -sample 1e-3 -threads 12 -binary 1
-./compute-accuracy vectors.bin 30000 < questions-words.txt
-# to compute accuracy with the full vocabulary, use: ./compute-accuracy vectors.bin < questions-words.txt
+
+echo -----------------------------------------------------------------------------------------------------
+echo -- distance...
+
+# to compute accuracy with the full vocabulary, use: compute-accuracy $VECTOR_DATA < $DATA_DIR/questions-words.txt
+set -x
+$BIN_DIR/compute-accuracy $VECTOR_DATA 30000 < $DATA_DIR/questions-words.txt
+
