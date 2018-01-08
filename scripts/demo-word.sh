551cd5d4191142bd636187ce4487592f7e63514c
Dav Yaginuma <dav@akuaku.org>
2018-01-07 21:24:27 -0800

    Merge in upstream updates from latest download of official source

--- scripts/demo-word.sh
+++ scripts/demo-word.sh
@@ -4,28 +4,10 @@ DATA_DIR=../data
 BIN_DIR=../bin
 SRC_DIR=../src
 
-TEXT_DATA=$DATA_DIR/text8
-ZIPPED_TEXT_DATA="${TEXT_DATA}.zip"
 VECTOR_DATA=$DATA_DIR/text8-vector.bin
 
 pushd ${SRC_DIR} && make; popd
+sh ./create-text8-vector-data.sh
 
-if [ ! -e $VECTOR_DATA ]; then
-  
-  if [ ! -e $TEXT_DATA ]; then
-    if [ ! -e $ZIPPED_TEXT_DATA ]; then
-	    wget http://mattmahoney.net/dc/text8.zip -O $ZIPPED_TEXT_DATA
-	fi
-    unzip $ZIPPED_TEXT_DATA
-	mv text8 $TEXT_DATA
-  fi
-  echo -----------------------------------------------------------------------------------------------------
-  echo -- Training vectors...
-  time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow 0 -size 200 -window 5 -negative 0 -hs 1 -sample 1e-3 -threads 12 -binary 1
-  
-fi
-
-echo -----------------------------------------------------------------------------------------------------
-echo -- distance...
-
-$BIN_DIR/distance $DATA_DIR/$VECTOR_DATA
+set -x
+$BIN_DIR/distance $VECTOR_DATA
